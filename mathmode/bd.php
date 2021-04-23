21c88991011e3fb79f2d4d6ba62b4aabde7b598a
Anna Tereshko <anna.tereshko@dev.logicsoftware.net>
2021-04-23 11:10:20 +0300

    fix: delete useless folder

--- mathmode/bd.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $db = mysqli_connect("localhost","root","26021711");
-    mysqli_select_db( $db,"mathmode");
-    ?>
--- mathmode/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['fullname']);
-unset($_SESSION['email']);
-unset($_SESSION['idusers']);
-unset($_SESSION['roleid']);
-session_destroy();
-header("location:login.php");
-?>
