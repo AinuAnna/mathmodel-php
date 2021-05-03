9993f71daa937bcb96681cc7588fee9101edf971
Anna Tereshko <anna.tereshko@dev.logicsoftware.net>
2021-05-03 15:44:16 +0300

    feat: add composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd20fa9866ce698faab487a25782a7e97c7a3ff8b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd20fa9866ce698faab487a25782a7e97c7a3ff8b',
+    ),
+  ),
+);
